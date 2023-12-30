cb8b7b1b7c6bf8fe0b5e96afe3d4aefd9d0e83e8
Fozzyack <fsundra@gmail.com>
2023-12-31 02:08:54 +0800

    testing revalidating route handlers again

--- src/app/api/weights/exercises/getexercises/route.ts
+++ src/app/api/weights/exercises/getexercises/route.ts
@@ -1,6 +1,6 @@
 import { pool } from "@/utils/db"
 
-export const dynamic = 'force-dynamic'
+export const revalidate = 60
 export async function GET(request: Request) {
     const sql = `SELECT exercises_weight.name AS e_name, muscle_groups.name AS m_name, exercises_weight.id FROM exercises_weight JOIN muscle_groups ON exercises_weight.muscle_group=muscle_groups.id ORDER BY exercises_weight.name ASC`
     const data = await pool.query(sql)
