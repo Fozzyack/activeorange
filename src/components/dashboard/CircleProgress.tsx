e7e79258571b252653ff15ba5799d515266c3df3
Fozzyack <fsundra@gmail.com>
2024-01-04 20:57:17 +0800

    formatting

--- src/components/dashboard/CircleProgress.tsx
+++ src/components/dashboard/CircleProgress.tsx
@@ -12,7 +12,7 @@ const CircleProgress = ({ mev, target }: { mev: number, target: number }) => {
         <div className='text-white rounded-xl relative'>
             <div className='rounded-full w-[100px] h-[100px] shadow-xl' >
                 <div className='bg-[#1B1F38]  w-full h-full rounded-full flex items-center justify-center' style={{ boxShadow: 'inset 0 10px 15px -3px rgb(0 0 0 / 0.1), inset 0 4px 6px -4px rgb(0 0 0 / 0.1)' }}>
-                    <p >{mev/target * 100}%</p>
+                    <p >{Math.round(mev/target * 100)}%</p>
                 </div>
 
             </div>
